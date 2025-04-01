86240ffd1d44db9289d533385b6bcd92cf83ac3f
dzungnguyen21 <22022546@vnu.edu.vn>
2025-04-02 00:30:49 +0700

    done 15 items

--- f2.py
+++ f2.py
@@ -14,31 +14,34 @@ def find_items(image_path, template_path, scale_range=(0.5, 1), scale_step=0.01)
     # Áp dụng Canny Edge Detection
     edges_image = cv2.Canny(gray_image, 50, 150)
     
-    best_match = None
-    best_val = -np.inf
-    best_rect = None
-    
+    best_matches = []
+
     # Duyệt qua các tỷ lệ
     for scale in np.arange(scale_range[0], scale_range[1], scale_step):
         resized_template = cv2.resize(gray_template, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
         edges_template = cv2.Canny(resized_template, 50, 200)
         
         result = cv2.matchTemplate(edges_image, edges_template, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        h, w = resized_template.shape
         
-        if max_val > best_val:
-            best_val = max_val
-            best_match = max_loc
-            h, w = resized_template.shape
-            best_rect = (best_match, (best_match[0] + w, best_match[1] + h))
+        # Lấy top 10 vị trí khớp nhất
+        num_results = 10
+        locs = np.dstack(np.unravel_index(np.argsort(result.ravel())[::-1], result.shape))[:num_results]
+        
+        for loc in locs[0]:
+            best_matches.append((result[loc[0], loc[1]], (loc[1], loc[0]), (loc[1] + w, loc[0] + h)))
+    
+    # Sắp xếp lại danh sách dựa trên giá trị matching score
+    best_matches.sort(reverse=True, key=lambda x: x[0])
+    best_matches = best_matches[:10]  # Chỉ giữ lại top 10
     
     # Vẽ hình chữ nhật lên vị trí tìm được tốt nhất
-    if best_rect:
-        cv2.rectangle(image, best_rect[0], best_rect[1], (0, 255, 0), 2)
+    for match in best_matches:
+        cv2.rectangle(image, match[1], match[2], (0, 255, 0), 2)
     
     # Hiển thị kết quả
     cv2.imshow('Detected Items', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-find_items('Finding/12.jpg', 'Template/find/1/baloon.jpg')
+find_items('Counting/cat.jpg', 'Template/count/cat.jpg')
